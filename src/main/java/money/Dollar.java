88127f7b0bdad7db37bd55b83255903ccfb1f235
okoysm <fly.child1017@gmail.com>
2018-01-27 08:58:28 +0900

    finish chapter1

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -1,4 +1,13 @@
 package money;
 
 public class Dollar {
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    void times(int multiplier) {
+        amount *= multiplier;
+    }
 }
