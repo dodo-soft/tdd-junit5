261e4b0b424d2da589964f9e77476430d338ca6d
okoysm <fly.child1017@gmail.com>
2018-01-27 11:29:11 +0900

    TODOをコメントで追加

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
+    //TODO:$5 + 10CHF = $10 (レートが2:1の場合)
+
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
@@ -17,4 +19,9 @@ public class MoneyTest {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
+
+    // TODO:hashCode()
+    // TODO:nullとの等価性比較
+    // TODO:他のオブジェクトとの等価性比較
+    // TODO:$5 + 10CHF = $10
 }
